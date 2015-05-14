3271526ad88c08c5cc8c72318404dc9151ee1c7e
RedRoosterMobile <info@redroostermobile.com>
2015-05-15 00:01:20 +0200

    Create getbtc.sh
    Getting current bitcoin.de price via imagemagick and tesseract OCR

---
+++ getbtc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 300x250
+curl -o btc_price.png https://bitcoinapi.de/widget/current-btc-price?size=3&type=dark
+echo "Waiting for curl.."
+sleep 10
+echo "Cutting BTC price.."
+convert -colorspace Gray  btc_price.png -crop 180x50+75+20  +repage output_btc.png
+echo "Running OCR-scan.."
+tesseract output_btc.png btc_price
+cat btc_price.txt
+rm btc_price.png
+rm output_btc.png
